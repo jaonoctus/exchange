1f7b14e6bc14da107f2e4e95c4d47e527347b296
Guiller <ghminvest@gmail.com>
2019-09-03 23:06:06 -0300

    for / router trading

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('trading', 'TradingController@index')->name('trading');
+Route::get('/trading', 'TradingController@index')->name('trading');
